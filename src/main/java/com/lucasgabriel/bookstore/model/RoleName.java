1e915e2bcf650eebaa2529296749d26634a823f3
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2020-02-19 23:06:32 -0300

    continuidade do backend

---
+++ src/main/java/com/lucasgabriel/bookstore/model/RoleName.java
@@ -0,0 +1,6 @@
+package com.lucasgabriel.bookstore.model;
+
+public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/lucasgabriel/bookstore/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.lucasgabriel.bookstore.repository;
+
+import com.lucasgabriel.bookstore.model.Role;
+import com.lucasgabriel.bookstore.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
